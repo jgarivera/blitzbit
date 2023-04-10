6fc27ef63b48099f2092c444e10d5f34e09b4b58
Jose Gabrielle Rivera <rivera.jga10@gmail.com>
2023-04-10 17:48:41 +0800

    fix: unproject mouse click position in spawn minion action

--- core/src/main/java/com/blitzbit/core/input/actions/SpawnMinion.java
+++ core/src/main/java/com/blitzbit/core/input/actions/SpawnMinion.java
@@ -1,16 +1,19 @@
 package com.blitzbit.core.input.actions;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.math.Vector3;
 import com.blitzbit.core.entity.Minion;
 import com.blitzbit.core.input.Action;
 import com.blitzbit.core.world.GameWorld;
 
 public class SpawnMinion implements Action {
+
     public boolean enter(GameWorld world) {
         float x = Gdx.input.getX();
         float y = Gdx.input.getY();
 
-        world.spawnEntity(new Minion(x, y));
+        Vector3 mouseClickPosition = world.getCamera().unproject(new Vector3(x, y, 0));
+        world.spawnEntity(new Minion(mouseClickPosition.x, mouseClickPosition.y));
 
         return true;
     }
