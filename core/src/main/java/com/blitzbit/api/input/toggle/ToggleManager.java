0b8beef05b2da8868232d5f40ab9af9993a8f7fd
Jose Gabrielle Rivera <rivera.jga10@gmail.com>
2023-04-13 18:00:48 +0800

    feat: make toggle manager api abstract

--- core/src/main/java/com/blitzbit/api/input/toggle/ToggleManager.java
+++ core/src/main/java/com/blitzbit/api/input/toggle/ToggleManager.java
@@ -2,13 +2,16 @@ package com.blitzbit.api.input.toggle;
 
 import java.util.HashMap;
 
-public class ToggleManager {
+public abstract class ToggleManager {
 
     private final HashMap<String, Boolean> toggles;
 
     public ToggleManager() {
         toggles = new HashMap<>();
+        registerToggles();
     }
+
+    protected abstract void registerToggles();
 
     public void setToggle(String name, boolean value) {
         toggles.put(name, value);
--- core/src/main/java/com/blitzbit/internal/input/GameToggleManager.java
+++ core/src/main/java/com/blitzbit/internal/input/GameToggleManager.java
@@ -6,10 +6,11 @@ public class GameToggleManager extends ToggleManager {
     public static final String DEBUG_MODE = "debug";
 
     public GameToggleManager() {
-        initialize();
+        super();
     }
 
-    private void initialize() {
+    @Override
+    protected void registerToggles() {
         setToggle(DEBUG_MODE, false);
     }
 }
