41cb25a7fecd47b2755adf12f2c79b968db5183d
Jose Gabrielle Rivera <rivera.jga10@gmail.com>
2023-04-13 17:59:38 +0800

    feat: make game screen api abstract

--- core/src/main/java/com/blitzbit/api/screens/GameScreen.java
+++ core/src/main/java/com/blitzbit/api/screens/GameScreen.java
@@ -3,7 +3,7 @@ package com.blitzbit.api.screens;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.ScreenAdapter;
 
-public class GameScreen extends ScreenAdapter {
+public abstract class GameScreen extends ScreenAdapter {
     protected final Game game;
 
     public GameScreen(Game game) {
