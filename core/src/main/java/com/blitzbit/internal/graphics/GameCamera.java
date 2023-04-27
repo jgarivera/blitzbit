689a51dc33089f5690c59ac11477c95e8373188c
Jose Gabrielle Rivera <rivera.jga10@gmail.com>
2023-04-27 21:26:07 +0800

    feat: implement zoom in game camera

--- core/src/main/java/com/blitzbit/internal/graphics/GameCamera.java
+++ core/src/main/java/com/blitzbit/internal/graphics/GameCamera.java
@@ -6,8 +6,10 @@ import com.blitzbit.api.graphics.Camera;
 public class GameCamera extends Camera {
     public static final float VIEWPORT_HEIGHT = 312;
     public static final float VIEWPORT_WIDTH = 312;
+    public static final float MIN_ZOOM = 0.5f;
+    public static final float MAX_ZOOM = 2.5f;
 
     public GameCamera() {
-        super(Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), VIEWPORT_WIDTH, VIEWPORT_HEIGHT);
+        super(Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), VIEWPORT_WIDTH, VIEWPORT_HEIGHT, MIN_ZOOM, MAX_ZOOM);
     }
 }
