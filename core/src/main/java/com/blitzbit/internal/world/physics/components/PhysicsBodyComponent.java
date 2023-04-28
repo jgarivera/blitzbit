f08b9341027868242cef11ad6385806c4dd49651
Jose Gabrielle Rivera <rivera.jga10@gmail.com>
2023-04-28 21:59:01 +0800

    feat: add body type field in physics body component

--- core/src/main/java/com/blitzbit/internal/world/physics/components/PhysicsBodyComponent.java
+++ core/src/main/java/com/blitzbit/internal/world/physics/components/PhysicsBodyComponent.java
@@ -2,15 +2,19 @@ package com.blitzbit.internal.world.physics.components;
 
 import com.badlogic.ashley.core.Component;
 import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
 
 public class PhysicsBodyComponent implements Component {
     public Body body;
 
-    public PhysicsBodyComponent() {
+    public BodyDef.BodyType bodyType;
 
+    public PhysicsBodyComponent(BodyDef.BodyType bodyType) {
+        this.bodyType = bodyType;
     }
 
-    public PhysicsBodyComponent(Body body) {
+    public PhysicsBodyComponent(Body body, BodyDef.BodyType bodyType) {
         this.body = body;
+        this.bodyType = bodyType;
     }
 }
