82ce1333b09401c358fb3db76661603b1fa2de15
Jose Gabrielle Rivera <rivera.jga10@gmail.com>
2023-04-28 18:59:25 +0800

    refactor: use screen and viewport static variables

--- core/src/main/java/com/blitzbit/internal/graphics/GameCamera.java
+++ core/src/main/java/com/blitzbit/internal/graphics/GameCamera.java
@@ -4,12 +4,14 @@ import com.badlogic.gdx.Gdx;
 import com.blitzbit.api.graphics.Camera;
 
 public class GameCamera extends Camera {
-    public static final float VIEWPORT_HEIGHT = 312;
-    public static final float VIEWPORT_WIDTH = 312;
     public static final float MIN_ZOOM = 0.5f;
     public static final float MAX_ZOOM = 2.5f;
+    private static final float SCREEN_WIDTH = Gdx.graphics.getWidth();
+    public static final float VIEWPORT_WIDTH = 312;
+    private static final float SCREEN_HEIGHT = Gdx.graphics.getHeight();
+    public static final float VIEWPORT_HEIGHT = 312;
 
     public GameCamera() {
-        super(Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), VIEWPORT_WIDTH, VIEWPORT_HEIGHT, MIN_ZOOM, MAX_ZOOM);
+        super(SCREEN_WIDTH, SCREEN_HEIGHT, VIEWPORT_WIDTH, VIEWPORT_HEIGHT, MIN_ZOOM, MAX_ZOOM);
     }
 }
