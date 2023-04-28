716b4b9692f1fae267c7d877f4ee4a4c8d252d9b
Jose Gabrielle Rivera <rivera.jga10@gmail.com>
2023-04-28 18:57:52 +0800

    feat: set desktop window size to 720p

--- desktop/src/main/java/com/blitzbit/DesktopLauncher.java
+++ desktop/src/main/java/com/blitzbit/DesktopLauncher.java
@@ -2,17 +2,18 @@ package com.blitzbit;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import com.blitzbit.BlitzbitGame;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
+
     public static void main(String[] arg) {
         Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 
         config.setTitle("Blitzbit");
         config.setForegroundFPS(60);
         config.useVsync(false);
-        config.setWindowSizeLimits(640, 480, 640, 480);
+        config.setWindowedMode(1280, 720);
+        config.setWindowSizeLimits(1280, 720, 1920, 1080);
 
         new Lwjgl3Application(new BlitzbitGame(), config);
     }
