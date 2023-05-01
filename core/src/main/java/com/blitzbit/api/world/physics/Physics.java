6367db6d491c359e8000611debca653fae633bd1
Jose Gabrielle Rivera <rivera.jga10@gmail.com>
2023-05-01 16:30:56 +0800

    feat: call box2d init in physics api constructor

--- core/src/main/java/com/blitzbit/api/world/physics/Physics.java
+++ core/src/main/java/com/blitzbit/api/world/physics/Physics.java
@@ -2,6 +2,7 @@ package com.blitzbit.api.world.physics;
 
 import com.badlogic.gdx.math.Matrix4;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Box2D;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.World;
 
@@ -10,6 +11,7 @@ public class Physics {
     private final Box2DDebugRenderer debugRenderer;
 
     public Physics() {
+        Box2D.init();
         worldSimulation = new World(new Vector2(0, 0), true);
         debugRenderer = new Box2DDebugRenderer();
     }
