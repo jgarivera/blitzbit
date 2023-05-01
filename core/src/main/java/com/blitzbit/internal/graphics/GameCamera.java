df5ba099b59db518108a9d5333a5233cc1254ed5
Jose Gabrielle Rivera <rivera.jga10@gmail.com>
2023-05-01 17:44:44 +0800

    fix: use meters as viewport size unit

--- core/src/main/java/com/blitzbit/internal/graphics/GameCamera.java
+++ core/src/main/java/com/blitzbit/internal/graphics/GameCamera.java
@@ -6,10 +6,16 @@ import com.blitzbit.api.graphics.Camera;
 public class GameCamera extends Camera {
     public static final float MIN_ZOOM = 0.5f;
     public static final float MAX_ZOOM = 2.5f;
+    /**
+     * The viewport width in meters.
+     */
+    public static final float VIEWPORT_WIDTH = 10;
+    /**
+     * The viewport height in meters.
+     */
+    public static final float VIEWPORT_HEIGHT = 10;
     private static final float SCREEN_WIDTH = Gdx.graphics.getWidth();
-    public static final float VIEWPORT_WIDTH = 312;
     private static final float SCREEN_HEIGHT = Gdx.graphics.getHeight();
-    public static final float VIEWPORT_HEIGHT = 312;
 
     public GameCamera() {
         super(SCREEN_WIDTH, SCREEN_HEIGHT, VIEWPORT_WIDTH, VIEWPORT_HEIGHT, MIN_ZOOM, MAX_ZOOM);
