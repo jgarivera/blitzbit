0518c852eb86c1643270a275263daf32cd5505ac
Jose Gabrielle Rivera <rivera.jga10@gmail.com>
2023-05-02 20:03:58 +0800

    refactor: extract overlay information into own methods

--- core/src/main/java/com/blitzbit/internal/graphics/GameOverlay.java
+++ core/src/main/java/com/blitzbit/internal/graphics/GameOverlay.java
@@ -23,16 +23,21 @@ public class GameOverlay {
         int screenHeight = Gdx.graphics.getHeight();
 
         batch.begin();
+        renderFPSInformation(screenHeight);
+        renderCameraInformation(screenHeight);
+        batch.end();
+    }
 
+    private void renderFPSInformation(int screenHeight) {
         int fps = Gdx.graphics.getFramesPerSecond();
         font.draw(batch, String.format("FPS: %d", fps), 15, screenHeight - 15);
+    }
 
+    private void renderCameraInformation(int screenHeight) {
         Camera camera = world.getCamera();
         Vector3 cameraPos = camera.position;
         font.draw(batch, String.format("cam_x: %.2fm, cam_y: %.2fm", cameraPos.x, cameraPos.y), 15, screenHeight - 30);
         font.draw(batch, String.format("cam_zoom: %.2f", camera.zoom), 15, screenHeight - 45);
-
-        batch.end();
     }
 
     public void resize(int width, int height) {
