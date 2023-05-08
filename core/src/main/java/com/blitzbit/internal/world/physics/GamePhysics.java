9f3e0a08821f0bd14534a9bc226c350e18fbbc23
Jose Gabrielle Rivera <rivera.jga10@gmail.com>
2023-05-08 19:34:07 +0800

    refactor: format code

--- core/src/main/java/com/blitzbit/internal/world/physics/GamePhysics.java
+++ core/src/main/java/com/blitzbit/internal/world/physics/GamePhysics.java
@@ -5,7 +5,7 @@ import com.blitzbit.api.world.physics.Physics;
 public class GamePhysics extends Physics {
 
     private static final float UNIT = 32.0f;
-    public static final float PIXELS_TO_METERS = 1 / UNIT ;
+    public static final float PIXELS_TO_METERS = 1 / UNIT;
 
     public static float pixelsToMeters(float pixels) {
         return pixels * PIXELS_TO_METERS;
--- core/src/main/java/com/blitzbit/internal/world/view/components/RenderOrderComponent.java
+++ core/src/main/java/com/blitzbit/internal/world/view/components/RenderOrderComponent.java
@@ -3,7 +3,7 @@ package com.blitzbit.internal.world.view.components;
 import com.badlogic.ashley.core.Component;
 
 public class RenderOrderComponent implements Component {
- 
+
     public int zIndex;
 
     public RenderOrderComponent(int zIndex) {
